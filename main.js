7461ab43125cc02eb9bee7cd5e640e4a4d1d7139
Denis Tkachuk <denys.tkachuk@sperse.com>
2018-08-19 02:31:26 +0300

    add reset and shuffle

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let firstCard, secondCard;
 
 function flipCard() {
     if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -43,5 +44,17 @@ function unflipCards() {
         lockBoard = false;
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
