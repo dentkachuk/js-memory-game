72978d5fa90c36b2603ee79f3d1cb489757c6b84
Denis Tkachuk <denys.tkachuk@sperse.com>
2018-08-19 02:04:36 +0300

    add data-attr, add if statement

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     console.log(this);
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+        console.log({secondCard, firstCard});
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
