175c526466060ee019c07e0242e0c3daf34a5872
Denis Tkachuk <denys.tkachuk@sperse.com>
2018-08-19 01:55:49 +0300

    add animation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const cards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
     console.log(this);
-    this.classList.togle('flip');
+    this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
